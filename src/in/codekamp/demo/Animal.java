474216895abf4a3a46bc536b0e9af3d06e12d9bf
shubh <shubhamshukla.shukla@gmail.com>
2016-03-07 06:12:36 -0800

    third

--- src/in/codekamp/demo/Animal.java
+++ src/in/codekamp/demo/Animal.java
@@ -4,5 +4,5 @@ package in.codekamp.demo;
  * Created by HP-User on 3/7/2016.
  */
 public class Animal {
-    
+
 }
--- src/in/codekamp/demo/Demo.java
+++ src/in/codekamp/demo/Demo.java
@@ -2,6 +2,9 @@ package in.codekamp.demo;
 
 public class Demo {
     public static void main(String[] args) {
-
+Demo.welcome();
+    }
+    public static void welcome() {
+        System.out.println("welcome bro");
     }
 }
